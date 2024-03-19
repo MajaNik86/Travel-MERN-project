3920262c2208d89510ce46adad43e7456f762c92
MajaNik86 <majanik86@gmail.com>
2024-03-19 13:20:17 +0100

    added main header and main navigation bar with styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
-import { NewPlace } from "./places/pages/NewPlace";
+import  {NewPlace}  from "./places/pages/NewPlace";
+import {MainNavigation} from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
+    <>
+    <MainNavigation/>
+    <main>
     <Routes>
       <Route path="/" element={<Users />} />
       <Route path="/places/new" element={<NewPlace/>}/>
     </Routes>
+    </main>
+    </>
   );
 };
 
