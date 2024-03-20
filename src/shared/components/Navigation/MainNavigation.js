1a7bcc0cd2b663452dffb6d7e1e1b3a0401f4ac1
MajaNik86 <majanik86@gmail.com>
2024-03-20 09:55:07 +0100

    Added NavLinks and Sidedrawer for mobile navigation, placed in on seperate div inside index.html with React Portal also applied some styles to them

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import MainHeader from './MainHeader'
+import NavLinks from './NavLinks'
+import { SideDrawer } from './SideDrawer'
+import Backdrop from '../UIElements/Backdrop'
 import './MainNavigation.css'
 
 export const MainNavigation = () => {
-  return <MainHeader>
-    <button className='main-navigation__menu-btn'>
+const [drawerIsOpen,setDrowerIsOpen]=useState(false);
+
+const openDrawer=()=>{
+  setDrowerIsOpen(true);
+}
+const closeDrower=()=>{
+  setDrowerIsOpen(false);
+}
+
+  return <>
+ {drawerIsOpen && <Backdrop onClick={closeDrower}/>}
+  { drawerIsOpen && <SideDrawer className='.main-navigation__drawer-nav'>
+    <NavLinks/>
+  </SideDrawer> }
+   <MainHeader>
+    <button className='main-navigation__menu-btn' onClick={openDrawer}>
         <span/>
         <span/>
         <span/>
     </button>
     <h1 className='main-navigation__title'>
         <Link to='/'>Your places</Link> </h1>
-    <nav>
-        ...
+    <nav className='main-navigation__header-nav'>
+        <NavLinks/>
     </nav>
   </MainHeader>
+  </>
+
 }
