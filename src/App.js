e611606c5529b148f995a5d77976f9f447d0d851
MajaNik86 <majanik86@gmail.com>
2024-03-20 10:59:21 +0100

    added user places page and components places list and place item in order to displat dummy places, added styles and a route in app js for user places

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
 import  {NewPlace}  from "./places/pages/NewPlace";
 import {MainNavigation} from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <main>
     <Routes>
       <Route path="/" element={<Users />} />
+      <Route path="/:userId/places" element={<UserPlaces/>}></Route>
       <Route path="/places/new" element={<NewPlace/>}/>
     </Routes>
     </main>
