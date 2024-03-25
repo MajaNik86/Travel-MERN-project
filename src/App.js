33c43de6d698f6a7153743667c7e9de36d2066a2
MajaNik86 <majanik86@gmail.com>
2024-03-25 14:49:58 +0100

    added UpdatePlace page with entered values and changed in input componant values if there is something entered, fixed the styles to be the same on new place and add place page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
 import  NewPlace from "./places/pages/NewPlace";
 import {MainNavigation} from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Users />} />
       <Route path="/:userId/places" element={<UserPlaces/>}></Route>
       <Route path="/places/new" element={<NewPlace/>}/>
+      <Route path="/places/:placeId" element={<UpdatePlace/>}/>
     </Routes>
     </main>
     </>
