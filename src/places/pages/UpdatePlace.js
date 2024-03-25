0065b83c4fda43362801dc138dd276f117347d21
MajaNik86 <majanik86@gmail.com>
2024-03-25 16:46:14 +0100

    fixed some bugs on updatePlace page

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
@@ -7,6 +7,8 @@ import {
   VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
+import Card from "../../shared/components/UIElements/Card";
+
 import "./PlaceForm.css";
 
 const DUMMY_PLACES = [
@@ -25,7 +27,7 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Empire State Building",
+    title: "Emp.st.Building",
     description: "One of the most famous sky scrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
@@ -39,40 +41,44 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
-  const[isLoding,setIsLoading]= useState(true);
+  const [isLoding, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
   // const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler, setFormData ] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: '',
+        value: "",
         isValid: false,
       },
       description: {
-        value: '', 
+        value: "",
         isValid: false,
       },
     },
     false
   );
-    const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
-    
-    useEffect(()=>{
-      setFormData({
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
-        },
-        description: {
-          value: identifiedPlace.description, 
-          isValid: true,
-        },
-      },true);
-      setIsLoading(false)
-    },[setFormData,identifiedPlace])
-   
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
+  useEffect(() => {
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
+        },
+        true
+      );
+    }
+
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
 
   const placeUpdateSubmitHandler = (event) => {
     event.preventDefault();
@@ -82,12 +88,14 @@ const UpdatePlace = () => {
   if (!identifiedPlace) {
     return (
       <div className="center">
+        <Card>
         <h2>Could not find place!</h2>
+        </Card>
       </div>
     );
   }
 
-  if(isLoding){
+  if (isLoding) {
     return (
       <div className="center">
         <h2>Loading...</h2>
@@ -95,7 +103,7 @@ const UpdatePlace = () => {
     );
   }
   return (
-     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
