08081e269e088284ca1156385b0f62023a4c9d5e
MajaNik86 <majanik86@gmail.com>
2024-03-25 13:36:38 +0100

    added more input components to newPLace.js implemented logic with useReducer,secured no infitite loops occur with useCallback, added useEffect to input component, and for now just conosle.log form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
-import  {NewPlace}  from "./places/pages/NewPlace";
+import  NewPlace from "./places/pages/NewPlace";
 import {MainNavigation} from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 
